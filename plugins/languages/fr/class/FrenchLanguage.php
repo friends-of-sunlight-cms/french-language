73b614c6bc6660667d249bde77e57967505015a0
Helwa Surinen <153928113+Helwa-Surinen@users.noreply.github.com>
2025-09-03 14:51:13 +0200

    Update FrenchLanguage.php

--- plugins/languages/fr/class/FrenchLanguage.php
+++ plugins/languages/fr/class/FrenchLanguage.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace SunlightLanguage\Fr;
+
+use Sunlight\Plugin\LanguagePlugin;
+
+class FrenchLanguage extends LanguagePlugin
+{
+    function formatInteger(int $integer): string
+    {
+        if ($integer > 9999) {
+            return parent::formatInteger($integer);
+        }
+
+        return sprintf('%d', $integer);
+    }
+
+    function formatFloat(float $float, int $decimals): string
+    {
+        if ($float > 9999) {
+            return parent::formatFloat($float, $decimals);
+        }
+
+        return number_format($float, $decimals, $this->data->options['decimal_point'], '');
+    }
+}
 
